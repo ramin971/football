86c76c5a6fc69c87ae22a21f76378b3de51db4d8
ramin <ramin.rahmani971@gmail.com>
2021-07-05 18:07:04 +0430

    Article models created in database and register

--- football/news/urls.py
+++ football/news/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 
-
+app_name = 'news'
 urlpatterns = [
     path('',views.home, name='home'),
 ]
--- football/news/views.py
+++ football/news/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request,'base.html',{})
+    return render(request, 'news/base.html', {})
